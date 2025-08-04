2cb00696bed64ce523c47745c5748024b7d187fd
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-08-04 18:31:49 +0000

    Deploying to gh-pages from @ AleksandraCherevko/TestVite@94ccf62ddd226626db3c0ad0ce8552858908b67e 🚀

---
+++ page-3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */document.querySelector(".feedback-form");const e={email:"",message:""};localStorage.setItem("formData",JSON.stringify(e));JSON.parse();
+//# sourceMappingURL=page-3.js.map
