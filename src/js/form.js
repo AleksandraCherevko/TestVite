f8875e474b4c28229f32267615294b9c710264cb
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-08-05 20:23:10 +0200

    Update form.js

--- src/js/form.js
+++ src/js/form.js
@@ -1,5 +1,44 @@
-const form = document.querySelector('.feedback-form');
-const formData = { email: '', message: '' };
+const STORAGE_KEY = 'feedback-form-state';
 
-localStorage.setItem('formData', JSON.stringify(formData));
-const parseFormData = JSON.parse()
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (savedData) {
+  emailInput.value = savedData.email || '';
+  messageInput.value = savedData.message || '';
+}
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  const { name, value } = event.target;
+  saveFormDataToStorage(name, value);
+}
+
+function saveFormDataToStorage(name, value) {
+  const formData = loadFormDataFromStorage();
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadFormDataFromStorage() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  return savedData ? JSON.parse(savedData) : {};
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { email, message } = loadFormDataFromStorage();
+
+  if (!email || !message) {
+    return alert('Будь ласка, заповніть обидва поля.');
+  }
+
+  console.log({ email, message });
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
